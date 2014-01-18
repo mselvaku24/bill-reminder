00330a73543a296948e1cbfd45addfa4b66ec6c1
mselvaku24 <mselvaku24@yahoo.com.au>
2014-01-18 15:46:43 +1000

    Added simple company using scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BillReminder::Application.config.secret_key_base = '15bc5e39bc51b16abfac0a351fcfed10d494fcebfefeb779f47d193e09b10969ff4db59100ab3377611661cf21e4a66e1edbd888189056752ed42772b16c383e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BillReminder::Application.config.session_store :cookie_store, key: '_bill-reminder_session'
